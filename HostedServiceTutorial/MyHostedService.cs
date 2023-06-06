fef8a4ff14bb01d74064049ed4084bd0512ca9d1
Prasadnair <prasadnair44@gmail.com>
2023-06-05 22:37:59 -0400

    hosted service sample

---
+++ HostedServiceTutorial/MyHostedService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+public class MyHostedService : IHostedService, IDisposable
+{
+    private readonly ILogger<MyHostedService> _logger;
+    private Timer _timer;
+
+    public MyHostedService(ILogger<MyHostedService> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("MyHostedService is starting.");
+
+        _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+        return Task.CompletedTask;
+    }
+
+    private void DoWork(object state)
+    {
+        _logger.LogInformation("Doing some work...");
+        // Perform your background processing or task here
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("MyHostedService is stopping.");
+
+        _timer?.Change(Timeout.Infinite, 0);
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _timer?.Dispose();
+    }
+}
---
+++ HostedServiceTutorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var host = new HostBuilder()
+.ConfigureServices((hostContext, services) =>
+{
+    services.AddHostedService<MyHostedService>();
+})
+.ConfigureLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.AddConsole();
+})
+.Build();
+
+await host.RunAsync();
